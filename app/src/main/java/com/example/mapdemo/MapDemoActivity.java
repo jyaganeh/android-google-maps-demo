e8385e04f0360cdacf62159aea4d6dce8c22153d
Roger Hu <roger.hu@gmail.com>
2018-05-01 23:05:32 -0700

    Add annotation

--- app/src/main/java/com/example/mapdemo/MapDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/MapDemoActivity.java
@@ -98,13 +98,12 @@ public class MapDemoActivity extends AppCompatActivity {
         MapDemoActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
+    @SuppressWarnings({"MissingPermission"})
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
     void getMyLocation() {
-        //noinspection MissingPermission
         map.setMyLocationEnabled(true);
 
         FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
-        //noinspection MissingPermission
         locationClient.getLastLocation()
                 .addOnSuccessListener(new OnSuccessListener<Location>() {
                     @Override
